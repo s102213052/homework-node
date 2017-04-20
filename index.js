92de49fe1216f914199f5b25d77c244e55abd3ef
tcs9407 <tcs9407@gmail.com>
2017-04-20 19:49:14 +0800

    homework

--- index.js
+++ index.js
@@ -3,10 +3,16 @@ var request = require('request');
 http.createServer(function(req,res){
 	res.setHeader('Content-Type', 'text/html');
 	res.writeHead(200);
-	res.write('hello world');
-	request.post({url:'http://ycchen.im.ncnu.edu.tw/join.php', form: {name:'Mr. Nobody',sex:'male'}}, function(err,httpResponse,body){
+	
+	request.post(
+	    {
+			url:'http://ycchen.im.ncnu.edu.tw/join.php', 
+			form: {name:'Mr. Nobody',sex:'male'
+		}
+	}, function(err,httpResponse,body){
 		
-		console.log(httpResponse);
+		res.write(body);
+		res.end();
 	});
-	res.end();
+	
 }).listen(7774);
